58eded44955249ba4c0bd772520554e51b4ac2a5
Takeitiz <tuandat8103@gmail.com>
2024-10-27 08:30:24 +0700

    fix: add feature so that client can call server by ip address not localhost

--- project1a/client/src/main/java/com/example/client/service/ApiService.java
+++ project1a/client/src/main/java/com/example/client/service/ApiService.java
@@ -8,6 +8,12 @@ import org.springframework.http.ResponseEntity;
 @Service
 public class ApiService {
 
+
+    // Thay bang dia chi IP cua server
+    private static final String IP_ADDRESS = "192.168.1.xxx";
+    private static final String PORT = "8080";
+    private static final String SERVER_URL = "http://" + IP_ADDRESS + ":" + PORT + "/response";
+
     private final RestTemplate restTemplate;
 
     public ApiService(RestTemplate restTemplate) {
@@ -17,6 +23,16 @@ public class ApiService {
     public ResponseDto callServerApi() {
         String url = "http://localhost:8080/response";
         ResponseEntity<ResponseDto> response = restTemplate.getForEntity(url, ResponseDto.class);
+        System.out.println("Connecting to server at: " + SERVER_URL);
+        if (response.getStatusCode().is2xxSuccessful() && response.getBody() != null) {
+            ResponseDto data = response.getBody();
+            System.out.println("Received data:");
+            System.out.println("Device ID: " + data.getDeviceId());
+            System.out.println("QR Code ID: " + data.getQrCodeId());
+            System.out.println("QR Code Value: " + data.getQrCodeValue());
+        } else {
+            System.out.println("Error calling API: " + response.getStatusCode());
+        }
         return response.getBody();
     }
 }
